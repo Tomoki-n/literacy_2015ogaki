109ee66ddc2679054a077a36b7d9cf6c469a2edb
Fujisawa Tsutomu <rokugahanfuji@live.jp>
2015-09-19 22:37:07 +0900

    エンカウントシステムほぼ完成

---
+++ literacy_2015ogaki/DatabaseClass.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseClass.swift
+//  literacy_2015ogaki
+//
+//  Created by Fujisawa Tsutomu on 2015/09/19.
+//  Copyright (c) 2015年 Nishinaka Tomoki. All rights reserved.
+//
+
+import Foundation
