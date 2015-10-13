2006e90770b65e616fae6f570f7222a5c450c581
Nishinaka Tomoki <ariaqua713.f7z@gmail.com>
2015-10-13 17:05:22 +0900

    test

---
+++ literacy_2015ogaki/literacy_2015ogaki/inputViewController.swift
@@ -0,0 +1,9 @@
+//
+//  inputViewController.swift
+//  literacy_2015ogaki
+//
+//  Created by Nishinaka Tomoki on 2015/09/21.
+//  Copyright © 2015年 Nishinaka Tomoki. All rights reserved.
+//
+
+import Foundation
---
+++ literacy_2015ogaki/literacy_2015ogaki/literacy-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ${ProductModuleName}-Bridging-Header.h
+//  literacy_2015ogaki
+//
+//  Created by Nishinaka Tomoki on 2015/09/30.
+//  Copyright © 2015年 Nishinaka Tomoki. All rights reserved.
+//
+
+#ifndef __ProductModuleName__Bridging_Header_h
+#define __ProductModuleName__Bridging_Header_h
+
+
+#endif /* __ProductModuleName__Bridging_Header_h */
